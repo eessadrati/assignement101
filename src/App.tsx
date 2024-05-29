fb15e2f6a94ceffccae5566c55597a3bd12465c7
eessadrati <essadratielhassane@gmail.com>
2024-05-29 16:24:07 +0100

    add Shadcn and tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
+import { Button } from '@/components/ui/button';
 
 function App() {
-
   return (
     <div>
-      hello ther
+      <Button>Click me</Button>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
